4520eccc4cd28836de114c9f854b7f6d1e0e9938
Steven Lomon Lennartsson <steven.lennartsson@gmail.com>
2024-01-25 13:31:09 +0100

    Chatbot all set up in VSCode

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-# Example Flask route
 from flask import Flask, render_template, request
+from chatbot import start_chat_v2
 
 app = Flask(__name__)
 
@@ -9,12 +9,14 @@ def index():
     return render_template("index.html")
 
 
-@app.route("/predict", methods=["POST"])
-def predict():
-    # Get input from the form
-    input_data = request.form["input_data"]
+# @app.route("/predict", methods=["POST"])
+# def predict():
+#     # Get input from the form
+#     input_data = request.form["input_data"]
 
-    # Use your model to make predictions
-    prediction = your_model.predict(input_data)
+#     # Use your model to make predictions
+#     prediction = your_model.predict(input_data)
 
-    return render_template("result.html", prediction=prediction)
+#     return render_template("result.html", prediction=prediction)
+
+start_chat_v2()
